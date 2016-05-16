1edb62b5603b0eb26b169fd662de23fa87cfe52e
merdigon <szymon.nowak3848@gmail.com>
2016-05-16 15:09:38 +0200

    Added Gramma generator and 'Grammatica' parser generator

---
+++ Project/GrammaGenerator/GrammaGenerator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrammaGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.CurrentDirectory + @"\..\..\..\..\Grammatica\map.grammar";
+            using (StreamWriter strWriter = new StreamWriter(path))
+            {
+                StringBuilder strBuilder = new GrammaGenerator().CreateGramma();
+                strWriter.WriteLine(strBuilder.ToString());
+                strWriter.Flush();
+            }
+        }
+    }
+}
